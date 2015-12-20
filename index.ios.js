088a11a71066d44be2afb7c69c60e7f8a55ae7b8
Maxi Ferreira <charca@gmail.com>
2015-12-20 01:46:46 -0800

    Added login screen

--- index.ios.js
+++ index.ios.js
@@ -10,44 +10,80 @@ var {
   StyleSheet,
   Text,
   View,
+  Image,
+  TouchableHighlight
 } = React;
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000'
+  },
+  backgroundImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    opacity: 0.5
+  },
+  loginContainer: {
+    flex: 1,
+    backgroundColor: 'transparent'
+  },
+  loginHeader: {
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  loginButtons: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logoImage: {
+    width: 100,
+    height: 100
+  },
+  button: {
+    backgroundColor: '#21C064',
+    borderRadius: 28,
+    width: 200,
+    padding: 16
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 16,
+    // fontFamily: 'Open Sans',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+});
 
 var SocialApp = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Image
+          style={styles.backgroundImage}
+          source={require('./img/background.png')} />
+        <View style={styles.loginContainer}>
+          <View style={styles.loginHeader}>
+            <Image
+              style={styles.logoImage}
+              source={require('./img/logo.png')} />
+          </View>
+          <View style={styles.loginButtons}>
+            <TouchableHighlight
+              style={styles.button}
+              onPress={this.onLogin}>
+              <Text style={styles.buttonText}>LOG IN</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
       </View>
     );
   }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
 
 AppRegistry.registerComponent('SocialApp', () => SocialApp);
